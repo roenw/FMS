0243acd46364fe49d64cbea856cca056d418c4ef
Roen <13713430+roenw@users.noreply.github.com>
2022-10-17 23:25:13 -0700

    Create assets folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var bg;
 
 function preload(){
-    bg = loadImage("https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80")
+    bg = loadImage('assets/forest.jpg')
 }
 
 function setup() {
