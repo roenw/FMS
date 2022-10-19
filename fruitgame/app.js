dc41b85fe7699de0b52136850580d2643737d13c
Brendan <bwaldropw@aol.com>
2022-10-19 08:38:28 -0700

    back button implementation

--- fruitgame/app.js
+++ fruitgame/app.js
@@ -1,11 +1,18 @@
 var isRunning = false;
+var button;
 
 function setup() {
     createCanvas(1200, 900);
 
+    button = createImg('../assets/backbutton.png');
+    button.position(500, 500);
+    button.mousePressed(console.log("mouse press"));
+
     let score = new ScoreBoard(0);
     let time = new Timer(10);
     score.drawBoard();
+
+    
 
     score.updateBoard();
     score.setScore(10);
