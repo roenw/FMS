17d70017a5cc8a070be5c21fee80b1266e8ac95d
Roen <13713430+roenw@users.noreply.github.com>
2022-10-19 00:07:42 -0700

    Added monkey and mole images

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var bg;
 
 function preload() {
-    bg = loadImage('assets/forest.jpg')
+    bgImg = loadImage('assets/forest.jpg');
+    monkeyImg = loadImage('assets/monkey.png');
+    moleImg = loadImage('assets/mole.png');
 }
 
 function setup() {
   createCanvas(1200, 900);
-}
-
-function draw() {
   tint(150, 255);
-  background(bg);
+  background(bgImg);
   textSize(32);
   fill(255);
   text('Select a game to play!', 450, 40);
+  noTint();
+
+  var imgScale;
+
+  imgScale = 0.33;
+  image(monkeyImg, 150, 150, imgScale * 798, imgScale * 558);
+  
+  imgScale = 1.5;
+  image(moleImg, 600, 90, imgScale * 209, imgScale * 201);
+}
+
+function draw() {
 }
