b44bd9a5d17eb755e1d4e1b728ce8933b0a492ad
Brendan <bwaldropw@aol.com>
2022-10-20 15:05:37 -0700

    edit size

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(window.screen.width - 50, window.screen.height - 150);
+  createCanvas(window.screen.width - 100, window.screen.height - 200);
   tint(150, 255);
   background(bgImg);
   textFont(titleFont);
