39f2600ddcfee3660ebb23f7b38100e7cfd5c99b
Brendan <bwaldropw@aol.com>
2022-10-26 08:01:53 -0700

    menu adjustments

--- index.js
+++ index.js
@@ -44,14 +44,14 @@ function setup() {
   text('Select a game to play!', width / 4 + 50, 60);
   textSize(30);
   text('Paint Monkey', width * 0.20, height * 0.20);
-  text('Whack a Mole', width * 0.60 + 50, height * 0.20);
+  text('Whack a Mole', width * 0.60 + 30, height * 0.20);
   text('Sorting Shapes', width * 0.20, height * 0.60 + 50);
   text('Catch the Fruit', width * 0.60 + 25, height * 0.60 + 50);
   textSize(15);
   text('Kevin Zhang', width * 0.25, height * 0.50);
-  text('Roen Wainscoat', width * 0.65 + 25, height * 0.60 - 40);
+  text('Roen Wainscoat', width * 0.65 + 10, height * 0.60 - 40);
   text('Savannah Gong', width * 0.25, height * 0.90 + 50);
-  text('Brendan Waldrop', width * 0.65 + 25, height * 0.90 + 50);
+  text('Brendan Waldrop', width * 0.65 + 10, height * 0.90 + 50);
   noTint();
   pop();
 
@@ -61,7 +61,7 @@ function setup() {
   image(monkeyImg, width * 0.20, height * 0.20, imgScale * 798, imgScale * 558);
   
   imgScale = 1.5;
-  image(moleImg, width * 0.60, height * 0.20 - 50, imgScale * 209, imgScale * 201);
+  image(moleImg, width * 0.60 - 20, height * 0.20 - 50, imgScale * 209, imgScale * 201);
 
   imgScale = 1;
   image(sortedImg, width * 0.20, height * 0.70, imgScale * 223, imgScale * 160);
